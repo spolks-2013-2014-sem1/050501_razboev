275fd578ae03a0a6e93894f6d340d4deb12499f4
r <dima@r.(none)>
2013-09-18 21:35:06 +0300

    lab1 bad tcp.server ..

---
+++ lab1/1.rb
@@ -0,0 +1,10 @@
+﻿require 'socket'
+srvr = TCPServer.open('127.0.0.1',3000)
+while (server =srvr.accept)
+        string  = server.gets.chomp
+	server.print "/n/r#{string} /n/r"
+	puts "#{string}"
+        if string == "sd" 
+	  server.close 
+	end
+end
